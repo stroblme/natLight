556537f4e02f72e819ed4806c8d0f40fb9f325c5
Melvin Strobl <melvin.strobl@web.de>
2018-06-23 15:18:38 +0200

    - updated setup.py to new git url

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='natLight',
 	  version='0.1',
 	  description='Natural Ligth to RGB Converter',
-	  url='https://gitlab.com/stroblme/naturalLight2RGB.git',
+	  url='https://gitlab.com/stroblme/naturalLight.git',
 	  author='Melvin Strobl',
 	  packages=['natLight'],
 	  zip_safe=False)
