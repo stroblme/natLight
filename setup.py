c60b941562a35a0b16996d628ca9095e0cea369d
Melvin Strobl <melvin.strobl@web.de>
2020-04-12 20:08:44 +0200

    updated setup meta data

--- setup.py
+++ setup.py
@@ -4,9 +4,11 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='natLight',
-	  version='0.1',
-	  description='Natural Ligth to RGB Converter',
-	  url='https://gitlab.com/stroblme/naturalLight.git',
-	  author='Melvin Strobl',
-	  packages=['natLight'],
-	  zip_safe=False)
+	version='0.1',
+	description='Natural Ligth to RGB Converter',
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url='https://gitlab.com/stroblme/naturalLight.git',
+	author='Melvin Strobl',
+	packages=['natLight'],
+	zip_safe=False)
